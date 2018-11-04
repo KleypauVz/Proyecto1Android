9a8eb4e816beecb505452745014efd0679e46d15
Kleybert Carrillo <44482852+KleypauVz@users.noreply.github.com>
2018-11-03 21:07:41 -0400

    creamos los datos a utilizar en nuestra lista o array

---
+++ app/src/main/java/com/example/android/sweetlab/Datos.java
@@ -0,0 +1,24 @@
+package com.example.android.sweetlab;
+
+import java.io.Serializable;
+
+public class Datos implements Serializable {
+
+    private int chuche;
+    private String nombre;
+
+
+    public Datos(int chuche,String nombre){
+        this.chuche=chuche;
+        this.nombre=nombre;
+
+    }
+
+    public int getChuche() {
+        return chuche;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
